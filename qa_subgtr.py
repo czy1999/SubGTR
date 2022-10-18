d0e3788567a7a3e8db61e63a3baf3e058fe28174
czy1999 <1615186277@qq.com>
2022-10-18 15:35:29 +0800

    Add files via upload

---
+++ qa_subgtr.py
@@ -0,0 +1,226 @@
+import math
+import torch
+from torch import nn
+import numpy as np
+from tcomplex import TComplEx
+from transformers import DistilBertModel
+from torch.nn import LayerNorm
+
+class QA_SubGTR(nn.Module):
+    def __init__(self, tkbc_model, args):
+        super().__init__()
+        self.model = args.model
+        self.time_sensitivity = args.time_sensitivity
+        self.supervision = args.supervision
+        self.extra_entities = args.extra_entities
+        self.fuse = args.fuse
+        self.tkbc_embedding_dim = tkbc_model.embeddings[0].weight.shape[1]
+        self.sentence_embedding_dim = 768  # hardwired from
+
+        self.pretrained_weights = 'distilbert-base-uncased'
+        self.lm_model = DistilBertModel.from_pretrained(self.pretrained_weights)
+        if args.lm_frozen == 1:
+            print('Freezing LM params')
+            for param in self.lm_model.parameters():
+                param.requires_grad = False
+        else:
+            print('Unfrozen LM params')
+
+        # transformer
+        self.transformer_dim = self.tkbc_embedding_dim  # keeping same so no need to project embeddings
+        self.nhead = 8
+        self.num_layers = 6
+        self.transformer_dropout = 0.1
+        self.encoder_layer = nn.TransformerEncoderLayer(d_model=self.transformer_dim, nhead=self.nhead,
+                                                        dropout=self.transformer_dropout)
+        encoder_norm = LayerNorm(self.transformer_dim)
+        self.transformer_encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=self.num_layers,
+                                                         norm=encoder_norm)
+        self.project_sentence_to_transformer_dim = nn.Linear(self.sentence_embedding_dim, self.transformer_dim)
+        self.project_entity = nn.Linear(self.tkbc_embedding_dim, self.tkbc_embedding_dim)
+
+        # TKG embeddings
+        self.tkbc_model = tkbc_model
+        num_entities = tkbc_model.embeddings[0].weight.shape[0]
+        num_times = tkbc_model.embeddings[2].weight.shape[0]
+        ent_emb_matrix = tkbc_model.embeddings[0].weight.data
+        time_emb_matrix = tkbc_model.embeddings[2].weight.data
+
+        full_embed_matrix = torch.cat([ent_emb_matrix, time_emb_matrix], dim=0)
+        # +1 is for padding idx
+        self.entity_time_embedding = nn.Embedding(num_entities + num_times + 1,
+                                                  self.tkbc_embedding_dim,
+                                                  padding_idx=num_entities + num_times)
+        self.entity_time_embedding.weight.data[:-1, :].copy_(full_embed_matrix)
+        self.num_entities =num_entities
+        if args.frozen == 1:
+            print('Freezing entity/time embeddings')
+            self.entity_time_embedding.weight.requires_grad = False
+            for param in self.tkbc_model.parameters():
+                param.requires_grad = False
+        else:
+            print('Unfrozen entity/time embeddings')
+
+        # position embedding for transformer
+        self.max_seq_length = 100  # randomly defining max length of tokens for question
+        self.position_embedding = nn.Embedding(self.max_seq_length, self.tkbc_embedding_dim)
+        # print('Random starting embedding')
+        self.loss = nn.CrossEntropyLoss(reduction='mean')
+        self.layer_norm = nn.LayerNorm(self.transformer_dim)
+
+        self.linear = nn.Linear(768, self.tkbc_embedding_dim)  # to project question embedding
+        self.linearT = nn.Linear(768, self.tkbc_embedding_dim)  # to project question embedding
+        self.lin_cat = nn.Linear(3 * self.transformer_dim, self.transformer_dim)
+
+        self.linear1 = nn.Linear(self.tkbc_embedding_dim, self.tkbc_embedding_dim)
+        self.linear2 = nn.Linear(self.tkbc_embedding_dim, self.tkbc_embedding_dim)
+
+        self.dropout = torch.nn.Dropout(0.3)
+        self.bn1 = torch.nn.BatchNorm1d(self.tkbc_embedding_dim)
+        self.bn2 = torch.nn.BatchNorm1d(self.tkbc_embedding_dim)
+
+        return
+
+    def invert_binary_tensor(self, tensor):
+        ones_tensor = torch.ones(tensor.shape, dtype=torch.float32).cuda()
+        inverted = ones_tensor - tensor
+        return inverted
+
+    # scoring function from TComplEx
+    def score_time(self, head_embedding, tail_embedding, relation_embedding):
+        lhs = head_embedding
+        rhs = tail_embedding
+        rel = relation_embedding
+
+        time = self.tkbc_model.embeddings[2].weight
+
+        lhs = lhs[:, :self.tkbc_model.rank], lhs[:, self.tkbc_model.rank:]
+        rel = rel[:, :self.tkbc_model.rank], rel[:, self.tkbc_model.rank:]
+        rhs = rhs[:, :self.tkbc_model.rank], rhs[:, self.tkbc_model.rank:]
+        time = time[:, :self.tkbc_model.rank], time[:, self.tkbc_model.rank:]
+
+        return (
+                (lhs[0] * rel[0] * rhs[0] - lhs[1] * rel[1] * rhs[0] -
+                 lhs[1] * rel[0] * rhs[1] + lhs[0] * rel[1] * rhs[1]) @ time[0].t() +
+                (lhs[1] * rel[0] * rhs[0] - lhs[0] * rel[1] * rhs[0] +
+                 lhs[0] * rel[0] * rhs[1] - lhs[1] * rel[1] * rhs[1]) @ time[1].t()
+        )
+
+    def score_entity(self, head_embedding, tail_embedding, relation_embedding, time_embedding):
+
+        lhs = head_embedding[:, :self.tkbc_model.rank], head_embedding[:, self.tkbc_model.rank:]
+        rel = relation_embedding
+        time = time_embedding
+
+        rel = rel[:, :self.tkbc_model.rank], rel[:, self.tkbc_model.rank:]
+        time = time[:, :self.tkbc_model.rank], time[:, self.tkbc_model.rank:]
+
+        right = self.tkbc_model.embeddings[0].weight
+        # right = self.entity_time_embedding.weight
+        right = right[:, :self.tkbc_model.rank], right[:, self.tkbc_model.rank:]
+
+        rt = rel[0] * time[0], rel[1] * time[0], rel[0] * time[1], rel[1] * time[1]
+        full_rel = rt[0] - rt[3], rt[1] + rt[2]
+
+        return (
+                (lhs[0] * full_rel[0] - lhs[1] * full_rel[1]) @ right[0].t() +
+                (lhs[1] * full_rel[0] + lhs[0] * full_rel[1]) @ right[1].t()
+        )
+
+    def forward(self, a):
+        # Tokenized questions, where entities are masked from the sentence to have TKG embeddings
+        question_tokenized = a[0].cuda()  # torch.Size([1, 10])
+        question_attention_mask = a[1].cuda()
+        entities_times_padded = a[2].cuda()
+        entity_mask_padded = a[3].cuda()
+
+        # Annotated entities/timestamps
+        heads = a[4].cuda()
+        tails = a[5].cuda()
+        times = a[6].cuda()
+
+        t1 = a[7].cuda()
+        t2 = a[8].cuda()
+
+        # One extra entity for new before & after question type
+        tails2 = a[9].cuda()
+
+        # TKG embeddings
+        head_embedding = self.entity_time_embedding(heads)
+        tail_embedding = self.entity_time_embedding(tails)
+        tail_embedding2 = self.entity_time_embedding(tails2)
+        time_embedding = self.entity_time_embedding(times)
+
+        # Hard Supervision
+        t1_emb = self.tkbc_model.embeddings[2](t1)
+        t2_emb = self.tkbc_model.embeddings[2](t2)
+
+        # entity embeddings to replace in sentence
+        entity_time_embedding = self.entity_time_embedding(entities_times_padded)  # torch.Size([1, 10, 512])
+
+        # context-aware step
+        outputs = self.lm_model(question_tokenized, attention_mask=question_attention_mask)
+        last_hidden_states = outputs.last_hidden_state  # torch.Size([1, 10, 768])
+
+        # entity-aware step
+        # 768->512
+        question_embedding = self.project_sentence_to_transformer_dim(last_hidden_states)  # torch.Size([1, 10, 512])
+        entity_mask = entity_mask_padded.unsqueeze(-1).expand(question_embedding.shape)  # torch.Size([1, 10, 512])
+        masked_question_embedding = question_embedding * entity_mask  # set entity positions 0  # torch.Size([1, 10, 512])
+        # 512->512
+        entity_time_embedding_projected = self.project_entity(entity_time_embedding)  # torch.Size([1, 10, 512])
+
+        # time-aware step
+        time_pos_embeddings1 = t1_emb.unsqueeze(0).transpose(0, 1)  # torch.Size([1, 1, 512])
+        time_pos_embeddings1 = time_pos_embeddings1.expand(entity_time_embedding_projected.shape)  # torch.Size([1, 10, 512])
+
+        time_pos_embeddings2 = t2_emb.unsqueeze(0).transpose(0, 1)
+        time_pos_embeddings2 = time_pos_embeddings2.expand(entity_time_embedding_projected.shape)
+        if self.fuse == 'cat':
+            entity_time_embedding_projected = self.lin_cat(
+                torch.cat((entity_time_embedding_projected, time_pos_embeddings1, time_pos_embeddings2), dim=-1))
+        else:
+            entity_time_embedding_projected = entity_time_embedding_projected + time_pos_embeddings1 + time_pos_embeddings2  # torch.Size([1, 10, 512])
+
+        # Transformer information fusion layer
+        masked_entity_time_embedding = entity_time_embedding_projected * self.invert_binary_tensor(entity_mask)
+
+        combined_embed = masked_question_embedding + masked_entity_time_embedding  # torch.Size([1, 10, 512])
+        # also need to add position embedding
+        sequence_length = combined_embed.shape[1]
+        v = np.arange(0, sequence_length, dtype=np.long)
+        indices_for_position_embedding = torch.from_numpy(v).cuda()
+        position_embedding = self.position_embedding(indices_for_position_embedding)
+        position_embedding = position_embedding.unsqueeze(0).expand(combined_embed.shape)  # torch.Size([1, 10, 512])
+
+        combined_embed = combined_embed + position_embedding
+
+        combined_embed = self.layer_norm(combined_embed)
+        combined_embed = torch.transpose(combined_embed, 0, 1)
+
+        mask2 = ~(question_attention_mask.bool()).cuda()
+
+        output = self.transformer_encoder(combined_embed, src_key_padding_mask=mask2)  # torch.Size([10, 1, 512])
+
+        # Answer Predictions
+        relation_embedding = output[0]  # self.linear(output[0]) #cls token embedding # torch.Size([1, 512])
+        relation_embedding1 = self.dropout(self.bn1(self.linear1(relation_embedding)))
+        relation_embedding2 = self.dropout(self.bn1(self.linear2(relation_embedding)))
+
+        # Time sensitivity layer
+        if self.time_sensitivity:
+            scores_time1 = self.score_time(head_embedding, tail_embedding, relation_embedding1)
+            scores_time2 = torch.matmul(relation_embedding1, self.entity_time_embedding.weight.data[self.num_entities:-1, :].T) # cuz padding idx
+            scores_time = torch.maximum(scores_time1, scores_time2)
+        else:
+            scores_time = self.score_time(head_embedding, tail_embedding, relation_embedding1)
+
+        # if self.model == 'cronkgqa' or (
+        #         self.model == 'entityqr' and self.supervision != 'none'):  # supervision for cronkgqa and entityqr
+        #     time_embedding = (time_embedding + t1_emb + t2_emb) / 3  # just take the mean
+
+        scores_entity1 = self.score_entity(head_embedding, tail_embedding, relation_embedding2, time_embedding)
+        scores_entity2 = self.score_entity(tail_embedding, head_embedding, relation_embedding2, time_embedding)
+        scores_entity = torch.maximum(scores_entity1, scores_entity2)
+        scores = torch.cat((scores_entity, scores_time), dim=1)
+        return scores
---
+++ utils.py
@@ -0,0 +1,421 @@
+import pickle
+import random
+import torch
+import numpy as np
+from tcomplex import TComplEx, Tmodel, Rmodel
+
+from tqdm import tqdm
+
+
+def loadTkbcModel(tkbc_model_file):
+    print('Loading tkbc model from', tkbc_model_file)
+    x = torch.load(tkbc_model_file, map_location=torch.device("cpu"))
+    num_ent = x['embeddings.0.weight'].shape[0]
+    num_rel = x['embeddings.1.weight'].shape[0]
+    num_ts = x['embeddings.2.weight'].shape[0]
+    print('Number ent,rel,ts from loaded model:', num_ent, num_rel, num_ts)
+    sizes = [num_ent, num_rel, num_ent, num_ts]
+    rank = x['embeddings.0.weight'].shape[1] // 2  # complex has 2*rank embedding size
+    tkbc_model = TComplEx(sizes, rank, no_time_emb=False)
+    tkbc_model.load_state_dict(x)
+    tkbc_model.cuda()
+    print('Loaded tkbc model')
+    return tkbc_model
+
+
+def loadTkbcModel_complex(tkbc_model_file):
+    print('Loading complex tkbc model from', tkbc_model_file)
+    tcomplex_file = 'models/wikidata_big/kg_embeddings/tcomplex_17dec.ckpt'  # TODO: hack
+    tcomplex_params = torch.load(tcomplex_file)
+    complex_params = torch.load(tkbc_model_file)
+    num_ent = tcomplex_params['embeddings.0.weight'].shape[0]
+    num_rel = tcomplex_params['embeddings.1.weight'].shape[0]
+    num_ts = tcomplex_params['embeddings.2.weight'].shape[0]
+    print('Number ent,rel,ts from loaded model:', num_ent, num_rel, num_ts)
+    sizes = [num_ent, num_rel, num_ent, num_ts]
+    rank = tcomplex_params['embeddings.0.weight'].shape[1] // 2  # complex has 2*rank embedding size
+
+    # now put complex params in tcomplex model
+
+    tcomplex_params['embeddings.0.weight'] = complex_params['embeddings.0.weight']
+    tcomplex_params['embeddings.1.weight'] = complex_params['embeddings.1.weight']
+    torch.nn.init.xavier_uniform_(tcomplex_params['embeddings.2.weight'])  # randomize time embeddings
+
+    tkbc_model = TComplEx(sizes, rank, no_time_emb=False)
+    tkbc_model.load_state_dict(tcomplex_params)
+    tkbc_model.cuda()
+    print('Loaded complex tkbc model')
+    return tkbc_model
+
+
+def loadTmodel(tkbc_model_file):
+    print('Loading tkbc model from', tkbc_model_file)
+    x = torch.load(tkbc_model_file, map_location=torch.device("cpu"))
+    num_ent = x['embeddings.0.weight'].shape[0]
+    num_rel = x['embeddings.1.weight'].shape[0]
+    num_ts = x['embeddings.2.weight'].shape[0]
+    print('Number ent,rel,ts from loaded model:', num_ent, num_rel, num_ts)
+    sizes = [num_ent, num_rel, num_ent, num_ts]
+    rank = x['embeddings.0.weight'].shape[1] // 2  # complex has 2*rank embedding size
+    tkbc_model = Tmodel(sizes, rank, no_time_emb=False)
+    tkbc_model.load_state_dict(x)
+    tkbc_model.cuda()
+    print('Loaded tkbc model')
+    return tkbc_model
+
+
+def loadRmodel(tkbc_model_file):
+    print('Loading tkbc model from', tkbc_model_file)
+    x = torch.load(tkbc_model_file, map_location=torch.device("cpu"))
+    num_ent = x['embeddings.0.weight'].shape[0]
+    num_rel = x['embeddings.1.weight'].shape[0]
+    num_ts = x['embeddings.2.weight'].shape[0]
+    print('Number ent,rel,ts from loaded model:', num_ent, num_rel, num_ts)
+    sizes = [num_ent, num_rel, num_ent, num_ts]
+    rank = x['embeddings.0.weight'].shape[1] // 2  # complex has 2*rank embedding size
+    tkbc_model = Rmodel(sizes, rank, no_time_emb=False)
+    tkbc_model.load_state_dict(x)
+    tkbc_model.cuda()
+    print('Loaded tkbc model')
+    return tkbc_model
+
+
+def dataIdsToLiterals(d, all_dicts):
+    new_datapoint = []
+    id2rel = all_dicts['id2rel']
+    id2ent = all_dicts['id2ent']
+    id2ts = all_dicts['id2ts']
+    wd_id_to_text = all_dicts['wd_id_to_text']
+    new_datapoint.append(wd_id_to_text[id2ent[d[0]]])
+    new_datapoint.append(wd_id_to_text[id2rel[d[1]]])
+    new_datapoint.append(wd_id_to_text[id2ent[d[2]]])
+    new_datapoint.append(id2ts[d[3]])
+    new_datapoint.append(id2ts[d[4]])
+    return new_datapoint
+
+
+def getAllDicts(dataset_name):
+    # base_path = '/scratche/home/apoorv/tkbc/tkbc_env/lib/python3.7/site-packages/tkbc-0.0.0-py3.7.egg/tkbc/data/wikidata_small/'
+    base_path = 'data/{dataset_name}/kg/tkbc_processed_data/{dataset_name}/'.format(
+        dataset_name=dataset_name
+    )
+    dicts = {}
+    for f in ['ent_id', 'rel_id', 'ts_id']:
+        in_file = open(str(base_path + f), 'rb')
+        dicts[f] = pickle.load(in_file)
+    rel2id = dicts['rel_id']
+    ent2id = dicts['ent_id']
+    ts2id = dicts['ts_id']
+    file_ent = 'data/{dataset_name}/kg/wd_id2entity_text.txt'.format(
+        dataset_name=dataset_name
+    )
+    file_rel = 'data/{dataset_name}/kg/wd_id2relation_text.txt'.format(
+        dataset_name=dataset_name
+    )
+
+    def readDict(filename):
+        f = open(filename, 'r')
+        d = {}
+        for line in f:
+            line = line.strip().split('\t')
+            if len(line) == 1:
+                line.append('')  # in case literal was blank or whitespace
+            d[line[0]] = line[1]
+        f.close()
+        return d
+
+    e = readDict(file_ent)
+    r = readDict(file_rel)
+    wd_id_to_text = dict(list(e.items()) + list(r.items()))
+
+    def getReverseDict(d):
+        return {value: key for key, value in d.items()}
+
+    id2rel = getReverseDict(rel2id)
+    id2ent = getReverseDict(ent2id)
+    id2ts = getReverseDict(ts2id)
+
+    all_dicts = {'rel2id': rel2id,
+                 'id2rel': id2rel,
+                 'ent2id': ent2id,
+                 'id2ent': id2ent,
+                 'ts2id': ts2id,
+                 'id2ts': id2ts,
+                 'wd_id_to_text': wd_id_to_text
+                 }
+
+    return all_dicts
+
+
+def checkQuestion(question, target_type):
+    question_type = question['type']
+    if target_type != question_type:
+        return False
+    return True
+
+
+# def getDataPoint(question, all_dicts):
+
+def predictTime(question, model, all_dicts, k=1):
+    entities = list(question['entities'])
+    times = question['times']
+    target_type = 'simple_time'
+    if checkQuestion(question, target_type) == False:
+        print('Not Entity question')
+        return set()
+    ent2id = all_dicts['ent2id']
+    rel2id = all_dicts['rel2id']
+    id2ts = all_dicts['id2ts']
+    annotation = question['annotation']
+    head = ent2id[annotation['head']]
+    tail = ent2id[annotation['tail']]
+    # relation = rel2id[list(question['relations'])[0]]
+    relation = list(question['relations'])[0]
+    if 'P' not in relation:
+        relation = 'P' + relation
+    relation = rel2id[relation]  # + model.embeddings[1].weight.shape[0]//2 #+ 90
+    data_point = [head, relation, tail, 1, 1]
+    data_batch = torch.from_numpy(np.array([data_point])).cuda()
+    time_scores = model.forward_over_time(data_batch)
+    val, ind = torch.topk(time_scores, k, dim=1)
+    topk_set = set()
+    for row in ind:
+        for x in row:
+            topk_set.add(id2ts[x.item()][0])
+    return topk_set
+
+
+def predictTail(question, model, all_dicts, k=1):
+    entities = list(question['entities'])
+    times = list(question['times'])
+    target_type = 'simple_entity'
+    if checkQuestion(question, target_type) == False:
+        print('Not Entity question')
+        return set()
+    ent2id = all_dicts['ent2id']
+    rel2id = all_dicts['rel2id']
+    ts2id = all_dicts['ts2id']
+    id2ent = all_dicts['id2ent']
+    head = ent2id[entities[0]]
+    try:
+        time = ts2id[(times[0], 0, 0)]
+    except:
+        return set()
+    relation = list(question['relations'])[0]
+    if 'P' not in relation:
+        relation = 'P' + relation
+    relation = rel2id[relation]  # + model.embeddings[1].weight.shape[0]//2 #+ 90
+    data_point = [head, relation, 1, time, time]
+    data_batch = torch.from_numpy(np.array([data_point])).cuda()
+    predictions, factors, time = model.forward(data_batch)
+    val, ind = torch.topk(predictions, k, dim=1)
+    topk_set = set()
+    for row in ind:
+        for x in row:
+            topk_set.add(id2ent[x.item()])
+    return topk_set
+
+
+def checkIfTkbcEmbeddingsTrained(tkbc_model, dataset_name, split='test'):
+    filename = 'data/{dataset_name}/questions/{split}.pickle'.format(
+        dataset_name=dataset_name,
+        split=split
+    )
+    questions = pickle.load(open(filename, 'rb'))
+    all_dicts = getAllDicts(dataset_name)
+    for question_type in ['simple_entity', 'simple_time']:
+        correct_count = 0
+        total_count = 0
+        k = 1  # hit at k
+        for i in tqdm(range(len(questions))):
+            this_question_type = questions[i]['type']
+            if question_type == this_question_type and question_type == 'simple_entity':
+                which_question_function = predictTail
+            elif question_type == this_question_type and question_type == 'simple_time':
+                which_question_function = predictTime
+            else:
+                continue
+            total_count += 1
+            id = i
+            predicted = which_question_function(questions[id], tkbc_model, all_dicts, k)
+            intersection_set = set(questions[id]['answers']).intersection(predicted)
+            if len(intersection_set) > 0:
+                correct_count += 1
+
+        print(question_type, correct_count, total_count, correct_count / total_count)
+
+
+with open('./saved_pkl/e2rt.pkl', 'rb') as f:
+    e2rt = pickle.load(f)
+with open('./saved_pkl/event2time.pkl', 'rb') as f:
+    event2time = pickle.load(f)
+with open('./saved_pkl/e2tr.pkl', 'rb') as f:
+    e2tr = pickle.load(f)
+
+
+def get_neighbours(e):
+    tr = e2tr[e]
+    neighbours = []
+    for t in tr:
+        neighbours.append(t[0])
+        neighbours.append(t[2])
+    neighbours = set(neighbours)
+    neighbours.remove(e)
+    return list(neighbours)
+
+
+def check_triples(q1, q2):
+    l = []
+    if type(q2) == list:
+        for i in q2:
+            l += check_triples(q1, i)
+    else:
+        for i in e2rt[(q1, q2)]:
+            l.append(i)
+        for i in e2rt[(q2, q1)]:
+            l.append(i)
+    return l
+
+
+def rerank_ba(pred, entity_in_query, q_type, dataset):
+    vs = []
+    for p in pred:
+        related_triples = check_triples(p, list(entity_in_query))
+        v = 9999
+        if p in entity_in_query or len(related_triples) == 0:
+            pass
+        elif q_type == 'before':
+            for t in related_triples:
+                deta = dataset[7] - int(t[3]) - 1
+                if deta < v and deta >= 0:
+                    v = deta
+        else:
+            for t in related_triples:
+                deta = int(t[4]) - dataset[8] - 1
+                if deta < v and deta >= 0:
+                    v = deta
+        vs.append(v)
+    value_rank = np.argsort(vs)
+
+    return [pred[ix] for ix in value_rank]
+
+
+def rerank_fl(pred, entity_in_query, q_type, dataset):
+    vs = []
+    for p in pred:
+        related_triples = check_triples(p, list(entity_in_query))
+        v = 9999
+        if p in entity_in_query or len(related_triples) == 0:
+            pass
+        elif q_type == 'first':
+            for t in related_triples:
+                deta = int(t[3])
+                if deta < v:
+                    v = deta
+        else:
+            for t in related_triples:
+                deta = 9500 - int(t[4])
+                if deta < v and deta >= 0:
+                    v = deta
+        vs.append(v)
+    value_rank = np.argsort(vs)
+
+    return [pred[ix] for ix in value_rank]
+
+
+def rerank_st(pred, entity_in_query, q_type, dataset, event=False):
+    vs = []
+    ans = []
+    if len(entity_in_query) == 2:
+        trs = check_triples(list(entity_in_query)[0], list(entity_in_query)[1])
+        for t in trs:
+            ans.append(int(t[3]))
+            pred.append(int(t[3]))
+    for p in pred:
+        v = 9999
+        if p in ans:
+            v = 0
+        vs.append(v)
+    value_rank = np.argsort(vs)
+
+    return [pred[ix] for ix in value_rank]
+
+
+def rerank_tj(pred, entity_in_query, q_type, dataset, event=False):
+    vs = []
+    for p in pred:
+        related_triples = check_triples(p, list(entity_in_query))
+        v = 9999
+
+        if p not in entity_in_query and len(related_triples) != 0:
+            for tp in related_triples:
+                start = int(tp[3])
+                end = int(tp[4])
+                if not (start > dataset[8] or end < dataset[7]):
+                    v = 0
+        vs.append(v)
+    value_rank = np.argsort(vs)
+
+    return [pred[ix] for ix in value_rank]
+
+
+def subgraph_reasoning(dataset, index, question, predicted):
+    i = index
+    neighbours = []
+    # # ADD subgraph neighbours
+    # for e in question['entities']:
+    #     neighbours += get_neighbours(e)
+    # for neighbour in neighbours:
+    #     predicted.append(neighbour)
+    if question['type'] == 'before_after':
+        if 'event_head' in question['annotation'].keys():
+            event = question['annotation']['event_head']
+            if event[0] != 'Q':
+                t = int(event)
+            else:
+                t = int(list(event2time[event])[0][3])
+            d = list(dataset[i])
+            d[7] = t
+            d[8] = t
+            predicted = rerank_ba(predicted, question['entities'], question['annotation']['type'], d)[:k]
+        else:
+            predicted = rerank_ba(predicted, question['entities'], question['annotation']['type'],
+                                  dataset[i])
+
+    if question['type'] == 'first_last':
+        if question['answer_type'] == 'entity':
+            predicted = rerank_fl(predicted, question['entities'], question['annotation']['adj'],
+                                  dataset[i])
+
+    if question['type'] == 'simple_time':
+        predicted = rerank_st(predicted, question['entities'], None,
+                              dataset[i])
+
+    if question['type'] == 'time_join':
+        if len(question['entities']) == 2:
+            if 'event_head' in question['annotation'].keys():
+                event = question['annotation']['event_head']
+                if event[0] != 'Q':
+                    t = int(event)
+                else:
+                    t = int(list(event2time[event])[0][3])
+                d = list(dataset[i])
+                d[7] = t
+                d[8] = t
+                predicted = rerank_tj(predicted, question['entities'], None, d)[:k]
+            else:
+                predicted = rerank_tj(predicted, question['entities'], None,
+                                      dataset[i])
+    return predicted
+
+
+def print_info(args):
+    print('#######################')
+    print('Model: ' + args.model)
+    print('QA Dataset: ' + args.dataset_name)
+    print('Aware Module: ' + str(args.aware_module))
+    print('Subgraph Reasoning: ' + str(args.subgraph_reasoning))
+    print('Time Sensitivity: ' + str(args.time_sensitivity))
+    print('TKG Embeddings: ' + args.tkbc_model_file)
+    print('TKG for QA (if applicable): ' + args.tkg_file)
+    print('#######################')
